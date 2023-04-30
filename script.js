bb6afba38e2466b0334183510c10475cbf397257
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-04-30 14:30:59 +0600

    inital commit. navbar and hero section created. some responsive issue remained.

---
+++ script.js
@@ -0,0 +1,26 @@
+//Hamburger function
+
+//variables
+
+const navIcon = document.querySelector(".fa-bars-staggered");
+const menu = document.querySelector(".ul");
+console.log(menu);
+//EventListener and function
+navIcon.addEventListener("click", () => {
+  if (isBar(navIcon)) {
+    navIcon.classList.replace("fa-bars-staggered", "fa-xmark");
+    menu.style.right = "0%";
+  } else {
+    navIcon.classList.replace("fa-xmark", "fa-bars-staggered");
+    menu.style.right = "100%";
+  }
+});
+
+//Checking if navIcon is bar or xmark
+function isBar(icon) {
+  if (icon.classList.contains("fa-bars-staggered")) {
+    return true;
+  } else {
+    return false;
+  }
+}
